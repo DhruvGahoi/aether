63afc2ce35ec48d94e08937041f1624fc41bd82c
DhruvGahoi <dhruvgahoi11111@gmail.com>
2025-01-25 22:35:20 +0530

    feat: added literal tokens

--- scanner.c
+++ scanner.c
@@ -18,6 +18,10 @@ void initScanner(const char *source){
   scanner.line = 1;
 }
 
+static bool isDigit(char c){
+  return c >= '0' && c <= '9';
+}
+
 static bool isAtEnd() {
   return *scanner.current == '\0';
 }
@@ -88,12 +92,40 @@ static void skipWhitespace(){
   }
 }
 
+static Token number() {
+  while(!isDigit(peek())) advance();
+
+  // Look for a fractional part.
+  if(peek() == '.' && isDigit(peekNext())){
+    // Consume the "."
+    advance();
+    while(isDigit(peek())) advance();
+  }
+
+  return makeToken(TOKEN_NUMBER);
+}
+
+static Token string() {
+  while(peek() != '"' && !isAtEnd()){
+    if(peek() == '\n') scanner.line++;
+    advance();
+  }
+
+  if(isAtEnd()) return errorToken("Unterminated string.");
+
+  //The close quote.
+  advance();
+  return makeToken(TOKEN_STRING);
+}
+
 Token scanToken() {
   skipWhitespace();
   scanner.start = scanner.current;
   if(isAtEnd()) return makeToken(TOKEN_EOF);
 
   char c = advance();
+  if(isDigit(c)) return number();
+
   switch(c) {
     case '(': return makeToken(TOKEN_LEFT_PAREN);
     case ')': return makeToken(TOKEN_RIGHT_PAREN);
@@ -118,6 +150,7 @@ Token scanToken() {
     case '>':
       return makeToken(
           match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+    case '"': return string();
   }
 
   return errorToken("Unexpected character.");
